83dce627c19f3b7a22a8c9e32603746dd84ef665
bmclaughlin3 <b4mclaughlin@student.bridgew.edu>
2021-09-21 23:16:34 -0400

    Project 1

---
+++ src/com/company/File.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class File {
+    public class FileExample {
+
+        public static void main(String[] args) throws IOException {
+            var fileName = "students.txt";
+            var allLines = Files.readString(Paths.get(fileName));
+            var listOfNames= allLines.split("\n");
+            for (String name : listOfNames){
+                System.out.println(name);
+            }
+        }
+    }
+}
---
+++ src/com/company/Response.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Response {
+    public class Ex_3 {
+        public static void main(String[] args){
+            var studentNames = new ArrayList<String>();
+            var reader = new Scanner(System.in);
+            var userResponse = "";
+            while (!userResponse.equals("done")){
+                System.out.print("Enter next name or 'done' to end:");
+                userResponse = reader.nextLine();
+                if (!userResponse.equals("done")){
+                    studentNames.add(userResponse);
+                }
+            }
+            System.out.println("The students in your class are:");
+            for (var Name: studentNames){
+                System.out.println(Name);
+            }
+        }
+    }
+}
